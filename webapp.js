fbf97eb8d998ee3b4ef2f0a32c92d45bcd14dfbb
MORENO Sebastien - ISOCEL <sebastien.moreno@isocel.info>
2018-03-16 17:33:12 +0100

    feat: use port 8080

--- webapp.js
+++ webapp.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
-http.createServer(onRequest).listen(8888);
+http.createServer(onRequest).listen(8080);
 console.log('Server has started');
 
 function onRequest(request, response){
