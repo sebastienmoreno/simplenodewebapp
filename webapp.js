47657988b8a79eb4da0af10b5ffd835600c160b5
Sébastien Moreno <sebastien.moreno@thermofisher.com>
2019-10-10 14:30:17 +0200

    feat: add Dockerfile

--- webapp.js
+++ webapp.js
@@ -1,10 +1,11 @@
 var http = require('http')
+var os = require("os");
 
 http.createServer(onRequest).listen(8080);
 console.log('Server has started');
 
 function onRequest(request, response){
   response.writeHead(200);
-  response.write('Hello World of Noders!');
+  response.write('Hello World of Noders! Message from host: ' + os.hostname());
   response.end();
 }
